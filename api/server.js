fc8358bd2ad162228654ced5f390e3df63a47c48
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-13 18:03:46 -0500

    Baked chocolatechip COOKIES

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const Store = require("connect-session-knex")(session);
+
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,9 +22,34 @@ const cors = require("cors");
 
 const server = express();
 
+// Cookie Session and Store 
+server.use(session({
+  name: "chocolatechip",
+  secret: process.env.SESSION_SECRET || "keep it secret, keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+
+  store: new Store({
+    knex: require("../database/db-config"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}))
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
